aeec136cd55b752e78641d248db453f0452caea9
Tim Quimbo <tquimbo1@gmail.com>
2021-11-08 00:32:37 -0500

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  delete '/logout' => 'sessions#destroy'
+
   resources :reviews
   resources :books
   resources :users
