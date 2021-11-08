c1a76cad40309e97834779cbb1a3f51d2fc28781
Tim Quimbo <tquimbo1@gmail.com>
2021-11-07 23:54:32 -0500

    Model relationships.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+    belongs_to :user
+    belongs_to :book
+  
+    validates :rating, numericality: {greater_than: 0, less_than_or_equal_to: 5}
+    validates :comment, length: { minimum: 5 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-    
+    has_secure_password
+    has_many :reviews
+    has_many :books, through: :reviews
+    validates :username, :email, uniqueness: true
+    validates :username, :email, presence: true
 end
