5eef349bfd7051c662051ad2d5b3d5adae938860
Tim Quimbo <tquimbo1@gmail.com>
2021-11-08 00:43:45 -0500

    User controller methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,12 @@ def create
 end
 
 def show
-
+    redirect_if_not_logged_in
+    @user = User.find_by_id(params[:id])
+    redirect_to '/' if !@user
 end
+
+private
 
 def user_params
     params.require(:user).permit(:username, :email, :password)
