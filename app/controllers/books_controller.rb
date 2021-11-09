c85a5591838e72acf326ea261602a7571b9f84e1
Tim Quimbo <tquimbo1@gmail.com>
2021-11-08 23:41:44 -0500

    Book controller methods.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,29 @@
 class BooksController < ApplicationController
 
     def new 
-
+        @book = Book.new
     end
     
     def create
-    
+        @book = Book.new(book_params)
+        if @book.save
+            redirect_to book_path(@book)
+        else
+            render :new
+        end
     end
-    
+
     def show
-    
+        redirect_if_not_logged_in
+        @user = User.find_by_id(params[:id])
+        redirect_to '/' if !@user
+    end
+
+
+    private
+
+    def book_params
+        params.require(:book).permit(:name, :author, :genre)
     end
     
 
