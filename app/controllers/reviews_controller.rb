bdbf2c83c55bc561b3d3c83b65a2861c38b447c5
Tim Quimbo <tquimbo1@gmail.com>
2021-11-09 12:37:59 -0500

    Review controller methods.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,24 +2,33 @@ class ReviewsController < ApplicationController
 
     def new 
         @book = Book.find_by_id(params[:id])
-        @review = @book.build_review
+        @review = @book.reviews.build
     end
     
     def create
-    
+      @review = current_user.reviews.build(review_params)
+      if @review.save
+        redirect_to review_path(@review)
+      else
+        render :new
+      end
     end
     
     def show
+        @review = Review.find_by_id(params[:id])
+    end
     
+    def index
+      @reviews = Review.all
     end
 
-    def index
-        if !logged_in?
-          redirect_if_not_logged_in
-        else
-          redirect_to user_path(current_user)
-        end
-      end
+    private
+
+    def review_params
+        params.require(:review).permit( :comment, :rating )
+    end
+  
+  
 
 
     
