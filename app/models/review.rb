1818be9bb5a979bcac7fcae898de57c8e44be9c3
Tim Quimbo <tquimbo1@gmail.com>
2021-11-10 19:55:04 -0500

    Improved show review page.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,11 @@ class Review < ApplicationRecord
     belongs_to :book
 
     validates :rating, numericality: {greater_than: 0, less_than_or_equal_to: 10}, presence: true
+
+
+
+    validates :book, uniqueness: { scope: :user, message: "has already been reviewed by you"  }
+
   
   
 
