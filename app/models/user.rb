8a6870883d7d07733e66342a27429da8659f5ee1
Tim Quimbo <tquimbo1@gmail.com>
2021-11-29 23:05:08 -0500

    Add method to log in with google to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
     validates :username, uniqueness: true
     validates :username, presence: true
 
+    def self.create_by_google_omniauth(auth)
+        self.find_or_create_by(username: auth[:info][:email]) do |u|
+          u.password = SecureRandom.hex
+        end
+      end
+
+      
 end
